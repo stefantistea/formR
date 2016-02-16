9713333cb401cb6b682dbd7e226984fea8138b42
Ștefan Țîștea <stefantistea@yandex.com>
2016-02-16 07:07:04 +0200

    added the files

---
+++ dem.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){ 
+    $('#characterLeft').text('140 caractere ramase');
+    $('#message').keydown(function () {
+        var max = 140;
+        var len = $(this).val().length;
+        if (len >= max) {
+            $('#characterLeft').text('Ai scris prea mult, n-o sa citeasca nimeni atat.');
+            $('#characterLeft').addClass('red');
+            $('#btnSubmit').addClass('disabled');            
+        } 
+        else {
+            var ch = max - len;
+            $('#characterLeft').text(ch + ' characters left');
+            $('#btnSubmit').removeClass('disabled');
+            $('#characterLeft').removeClass('red');            
+        }
+    });    
+});
